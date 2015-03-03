875d7207d0e0219d86d2532c27a9acc3a1f41c2e
Loic Cadour <loic.cadour@inria.fr>
2015-03-03 11:54:30 +0100

    [PaintTBx] Breaks the link between the saved mask and the new one

--- src-plugins/medSegmentation/msegAlgorithmPaintToolbox.h
+++ src-plugins/medSegmentation/msegAlgorithmPaintToolbox.h
@@ -179,6 +179,7 @@ private:
     QLabel *m_brushRadiusLabel;
     QShortcut *addBrushSize_shortcut, *reduceBrushSize_shortcut;
     double m_strokeRadius;
+    bool maskHasBeenSaved;
     //
 
     // Magic Wand's objects
