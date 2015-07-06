6812c98ffe68c94b74ed72bf2232e8909290326d
mathilde.merle <mathilde.merle@ihu-liryc.fr>
2015-07-06 11:43:30 +0200

    [View Settings] improve layout spacing/margins

--- src/medCore/views/medAbstractView.cpp
+++ src/medCore/views/medAbstractView.cpp
@@ -393,6 +393,10 @@ QWidget* medAbstractView::navigatorWidget()
         navigatorLayout->setAlignment(Qt::AlignCenter);
 
         navigatorLayout->addWidget(primaryNavigator()->toolBoxWidget());
+        foreach (medAbstractNavigator* navigator, this->extraNavigators())
+        {
+            navigatorLayout->addWidget(navigator->toolBoxWidget()); // parameters for 3D view
+        }
     }
 
     return d->navigatorWidget;
